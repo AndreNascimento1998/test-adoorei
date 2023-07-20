5353192cceffb8c8450cb3a4eed6fbe9900ba391
André Linux <andre.ncardoso@hotmail.com>
2023-07-20 20:51:30 -0300

    feat: new interface for PlanList

---
+++ src/Interface/IPlainList.ts
@@ -0,0 +1,16 @@
+
+export interface IPlainList {
+    title: string
+    tip: number
+    desc: string
+    desc2: string
+    titleServers: string 
+    servers: Array<string>
+    titleApp: string
+    appAvaliable: Array<string>
+    migrationTitle?: string
+    migration?: Array<string>
+    titleMore: string
+    more: Array<string>
+    suport?: string
+}
