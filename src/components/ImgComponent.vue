c3bcc82b761bf768e8f46038376878db0f853cee
André Linux <andre.ncardoso@hotmail.com>
2023-07-20 20:50:57 -0300

    feat: new component img

---
+++ src/components/ImgComponent.vue
@@ -0,0 +1,14 @@
+<template>
+     <v-img 
+        :max-width="smAndUp ? '20%' : '50%'"
+        class="mb-6" 
+        :src="logo" 
+    />
+</template>
+
+<script setup lang="ts">
+import { useDisplay } from "vuetify/lib/framework.mjs";
+import logo from "@/assets/logo.png"
+
+const { smAndUp } = useDisplay()
+</script>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'
+import { createPinia } from 'pinia'
 
 loadFonts()
 
 const app = createApp(App)
   app.use(vuetify)
+  app.use(createPinia())
   app.use(router)
   app.mount('#app')
