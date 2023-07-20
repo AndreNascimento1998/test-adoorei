b70be8777d545b7df0d07ad6b552df18a269b30b
André Linux <andre.ncardoso@hotmail.com>
2023-07-20 16:52:30 -0300

    chore: new settings for v-btn and v-row

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
-    <v-app>
+    <v-app :theme="theme">
         <router-view />
     </v-app>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { useGlobalStore } from './stores/GlobalStore'
+
+const globalStore = useGlobalStore()
+
+const theme = computed(() => globalStore.theme)
+</script>
+
+<style>
+button.v-btn {
+    color: #F11A40;
+}
+
+.v-row{
+    flex: 0 0 auto;
+    margin: 0;
+}
+</style>
