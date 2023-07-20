912a8df63934b818dc66d3efb9418c68463cd5f7
André Linux <andre.ncardoso@hotmail.com>
2023-07-20 20:51:12 -0300

    feat: new compoment list

---
+++ src/components/ListComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-list 
+        lines="one" 
+        density="compact" 
+        class="pa-0" 
+        variant="outlined"
+        rounded
+    >
+        <h4 style="font-style: italic;" class="pa-2">{{ title }}</h4>
+        <v-list-item 
+            v-for="(item, index) in props.list" 
+            :key="index"
+            class="pa-2"
+        >
+            {{ item }}
+        </v-list-item>
+        <h4 
+            v-show="props.suport?.length" 
+            class="mt-4"
+        >
+            {{ props.suport }}
+        </h4>
+    </v-list>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    title: String,
+    list: Array,
+    suport: String
+})
+
+</script>
