7170139d141a7fbba1ad6ab6e6a7563c2fcde380
André Joarez <andre.ncardoso@hotmail.com>
2023-07-21 22:37:40 -0300

    feat: new page home, if log

---
+++ src/views/Home/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Area lagada</h1>
+    <v-btn @click="loggout">
+        Deslogar
+    </v-btn>
+</template>
+
+<script setup lang="ts">
+import router from "@/router";
+import HttpToken from "@/services/factory/Http/HttpToken";
+
+
+function loggout() {
+    HttpToken.clearToken()
+
+    router.push('/')
+}
+</script>
