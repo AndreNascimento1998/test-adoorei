6834abdd1c9b1e0d4751fbf5d5f7253149d1b7da
André Linux <andre.ncardoso@hotmail.com>
2023-07-23 00:26:49 -0300

    test: top-bar avatar

--- src/views/Home/HomePage.vue
+++ src/views/Home/HomePage.vue
@@ -1,24 +1,32 @@
 <template>
-    <v-row>
-        <v-col cols="12">
-            <h1>Area lagada</h1>
+    <v-container class="mt-16">
+        <v-row class="mt-4">
+        <v-col cols="12" sm="8" offset-sm="2">
+            <h1>Olá João,</h1>
+            <span>Seja bem vindo a sua conta de hospedagem.</span>
         </v-col>
-
-        <v-col cols="12" sm="6" offset-sm="3">
-            <iframe 
-                width="800" 
-                height="615" 
-                src="https://www.youtube.com/embed/FMweoEi9clA" 
-                title="YouTube video player" 
-                frameborder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                allowfullscreen>
-            </iframe>
+            <v-col cols="12" sm="8" offset-sm="2">
+                <iframe 
+                    :width="smAndUp ? 800 : 280" 
+                    :height="smAndUp ? 615 : 300" 
+                    src="https://www.youtube.com/embed/FMweoEi9clA" 
+                    title="YouTube video player" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    allowfullscreen>
+                </iframe>
+            </v-col>
+            <v-col cols="12" sm="8" offset-sm="2">
+            <span>Caso tenha alguma dúvida, <span style="text-decoration: underline;">visite nossa central de ajuda.</span></span>
         </v-col>
-
-    </v-row>
+        </v-row>
+    </v-container>
 </template>
 
 <script setup lang="ts">
+import { useDisplay } from "vuetify/lib/framework.mjs";
 
+
+
+const {smAndUp} = useDisplay()
 </script>
--- src/views/Menu/TopBar/TopBar.vue
+++ src/views/Menu/TopBar/TopBar.vue
@@ -1,34 +1,33 @@
 
 <template>
-        <v-app-bar elevation="1" class="pt-4 pa-2" height="62">
-            <v-col cols="8">
-            <ImgComponent
-            />
-            </v-col>
-            <v-col>
-                <v-menu bottom >
-                    <template #activator="{ props }">
-                        <v-btn icon v-bind="props">
-                            <v-icon size="45px">mdi-account-circle</v-icon>
-                        </v-btn>
-                    </template>
-                    <v-list>
-                        <v-list-item 
-                            v-for="(option, i) in options" 
-                            :key="i"
-                            @click="option.action"
-                        >
-                        <v-list-item-title 
-                        >
-                        <v-icon >
-                            {{ option.icon }}
-                        </v-icon> 
-                        {{ option.text }}
-                    </v-list-item-title>
-                        </v-list-item>
-                    </v-list>
-                </v-menu>
-            </v-col>
+        <v-app-bar elevation="1" class="pt-4 pa-1" height="62">
+              
+        <ImgComponent
+        />
+            
+            
+        <v-menu bottom >
+            <template #activator="{ props }">
+                <v-btn  class="ml-16" icon v-bind="props">
+                    <v-icon size="45px">mdi-account-circle</v-icon>
+                </v-btn>
+            </template>
+            <v-list>
+                <v-list-item 
+                    v-for="(option, i) in options" 
+                    :key="i"
+                    @click="option.action"
+                >
+                <v-list-item-title >
+                <v-icon>
+                    {{ option.icon }}
+                </v-icon > 
+                {{ option.text }}
+            </v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+            
         </v-app-bar>
 </template>
 
